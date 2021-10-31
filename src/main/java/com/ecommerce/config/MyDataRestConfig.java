fa37fae36e435779d3e005b6b03f7bc6052788f2
caglarcercinli <chaglar.ronin@gmail.com>
2021-10-31 13:34:16 +0100

    category json with id

--- src/main/java/com/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/ecommerce/config/MyDataRestConfig.java
@@ -8,8 +8,21 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -27,5 +40,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                         httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) ->
                         httpMethods.disable(theUnsupportedActions)));
+        //call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType tempEntityType:entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
